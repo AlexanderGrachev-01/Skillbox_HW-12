f3555ccbafc000a1a8fa231b20281a21814832f2
AlexanderGrachev-01 <sa89519011368@yandex.ru>
2022-04-22 15:08:35 +0300

    Initial Commit

---
+++ Skillbox_HW-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox_HW-12
+//
+//  Created by Александр  Грачев on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
