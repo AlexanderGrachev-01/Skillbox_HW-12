20a5fe327c661fbb82b089ceba5eec5ff82fba05
AlexanderGrachev-01 <sa89519011368@yandex.ru>
2022-04-27 17:35:40 +0300

    creatureVC and change request with alamofire

---
+++ Skillbox_HW-12/CreatureVC.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class CreatureVC: UIViewController {
+    
+    @IBOutlet weak var creatureImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- Skillbox_HW-12/CreaturesLoader.swift
+++ Skillbox_HW-12/CreaturesLoader.swift
@@ -6,26 +6,44 @@ class CreaturesLoader {
     var uerl = "https://rickandmortyapi.com/api/character"
     
     func loadedCreatures(completion: @escaping ([Creature]) -> Void) {
-        let url = URL(string: "https://rickandmortyapi.com/api/character")!
-        var request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data,
-               let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
-               let jsonDict = json as? NSDictionary {
-                print(json)
-                var creatures: [Creature] = []
-                for (_, data) in jsonDict {
-                    if let creature = Creature(data: data as! NSDictionary) {
-                        creatures.append(creature)
-                    }
-                }
-                DispatchQueue.main.async {
-                    completion(creatures)
+        AF.request("https://rickandmortyapi.com/api/character").responseJSON { response in
+            if let objects = response.result,
+                let jsonDict = objects as? NSDictionary {
+                 print(objects)
+                 var creatures: [Creature] = []
+                 for (_, data) in jsonDict {
+                     if let creature = Creature(data: data as! NSDictionary) {
+                         creatures.append(creature)
+                     }
+                 }
+                 DispatchQueue.main.async {
+                     completion(creatures)
                 }
             }
-                
         }
-        task.resume()
     }
 }
+        
+//        let url = URL(string: "https://rickandmortyapi.com/api/character")!
+//        var request = URLRequest(url: url)
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data,
+//               let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
+//               let jsonDict = json as? NSDictionary {
+//                print(json)
+//                var creatures: [Creature] = []
+//                for (_, data) in jsonDict {
+//                    if let creature = Creature(data: data as! NSDictionary) {
+//                        creatures.append(creature)
+//                    }
+//                }
+//                DispatchQueue.main.async {
+//                    completion(creatures)
+//                }
+//            }
+//
+//        }
+//        task.resume()
+//    }
+//}
 
